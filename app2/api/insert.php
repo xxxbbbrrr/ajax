8bf034b9b015ca3420adc9ba02b310b48da09bc7
xxxbbbrrr <xbr123@yahoo.com.tw>
2019-11-21 10:20:10 +0800

    用AJAX做新增資料功能

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,26 @@
 <?php
 //處理新增資料的請求
 
+include_once "base.php";
+// echo "伺服器已收到";
+// print_r($_POST);
+
+$uni_id=$_POST["uni_id"];
+$class_num=$_POST["class_num"];
+$name=$_POST["name"];
+$birthday=$_POST["birthday"];
+$nat_id=$_POST["nat_id"];
+$addr=$_POST["addr"];
+$parent=$_POST["parent"];
+$tel=$_POST["tel"];
+$dept=$_POST["dept"];
+$grad_at=$_POST["grad_at"];
+
+$sql="insert into students (`uni_id`, `class_num`, `name`, `birthday`, `nat_id`, `addr`, `parent`, `tel`, `dept`, `grad_at`) values('$uni_id', '$class_num', '$name', '$birthday', '$nat_id', '$addr', '$parent', '$tel', '$dept', '$grad_at')";
+
+// `uni_id`, `class_num`, `name`, `birthday`, `nat_id`, `addr`, `parent`, `tel`, `dept`, `grad_at`
+// '$uni_id', '$class_num', '$name', '$birthday', '$nat_id', '$addr', '$parent', '$tel', '$dept', '$grad_at'
+
+echo $pdo->exec($sql);
 
 ?>
